a7a2ccf921a6b7b7623631cd451af7c68656db2b
AniqueKhanABC <anique.khan@alphabetacore.com>
2024-04-22 11:59:43 +0500

    modified instagram clone in order to deploy to vps

--- django_instagram_clone/post/forms.py
+++ django_instagram_clone/post/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Post
 
 class NewPostForm(forms.ModelForm):
-    content = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple':True}),required=True)
+    content = forms.FileField(widget=forms.ClearableFileInput(attrs={'allow_multiple_selected':True}),required=True)
     caption = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}),required=True)
     tags = forms.CharField(widget=forms.TextInput(attrs={'data-role':'tagsinput'}),required=True)
 
--- django_instagram_clone/stories/forms.py
+++ django_instagram_clone/stories/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Story
 
 class NewStoryForm(forms.ModelForm):
-    content = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple':True}),required=True)
+    content = forms.FileField(widget=forms.ClearableFileInput(attrs={'allow_multiple_selected':True}),required=True)
     caption = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}),required=True)
 
     class Meta:
