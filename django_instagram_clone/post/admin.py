0ce0324628b620de52a4032d1e06ca0f1a085df9
AniqueKhan <aniquekhan004@gmail.com>
2024-10-24 17:08:50 +0500

    admin panel fixes

--- django_instagram_clone/post/admin.py
+++ django_instagram_clone/post/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from . import models
 # Register your models here.
+@admin.register(models.Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['user','caption']
 
-
-admin.site.register(models.Post)
 admin.site.register(models.Tag)
 admin.site.register(models.Follow)
 admin.site.register(models.Stream)
