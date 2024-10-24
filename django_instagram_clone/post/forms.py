3cd0fa2059e0b53b2c735ec0f501d9e85b41104f
AniqueKhan <aniquekhan004@gmail.com>
2024-10-24 15:56:18 +0500

    removed tags as required

--- django_instagram_clone/post/forms.py
+++ django_instagram_clone/post/forms.py
@@ -4,7 +4,7 @@ from .models import Post
 class NewPostForm(forms.ModelForm):
     content = forms.FileField(widget=forms.ClearableFileInput(attrs={'allow_multiple_selected':True}),required=True)
     caption = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}),required=True)
-    tags = forms.CharField(widget=forms.TextInput(attrs={'data-role':'tagsinput'}),required=True)
+    tags = forms.CharField(widget=forms.TextInput(attrs={'data-role':'tagsinput'}))
 
     class Meta:
         model = Post
